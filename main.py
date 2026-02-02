c34d1b9722470f1103010349066d72bc0bba7f0e
mhamdikhilaahmed-coder <mhamdi.khila.ahmed@iesllavaneres.org>
2026-02-02 22:10:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import discord, os, threading
+import discord
+import os
+import threading
 from discord.ext import commands
-from keep_alive import run
+from keep_alive import app
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -14,5 +16,13 @@ async def setup_hook():
     await bot.load_extension("cogs.tickets")
 
 bot.setup_hook = setup_hook
-threading.Thread(target=run).start()
-bot.run(os.environ["TOKEN"])
+
+def run_bot():
+    bot.run(os.environ["TOKEN"])
+
+if __name__ == "__main__":
+    print("Iniciando Flask + Discord bot")
+    threading.Thread(target=run_bot).start()
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
